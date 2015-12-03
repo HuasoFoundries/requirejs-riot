93eea76b4dce1d5ca2dbbaf94a34a14c6104b11d
Amenadiel <amenadiel@gmail.com>
2015-12-03 18:43:10 -0300

    jshint stuff

---
+++ example/build.js
@@ -0,0 +1,16 @@
+({
+
+    baseUrl: ".",
+
+    paths: {
+        "riot": "./lib/riot+compiler.min",
+        "riotloader": "../riotloader"
+    },
+
+    normalizeDirDefines: "all",
+
+    name: "app",
+    out: "app-built.js",
+    optimize: "none"
+
+})
